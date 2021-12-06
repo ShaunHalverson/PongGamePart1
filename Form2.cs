dcfcd5cf9f9b5e3dba7a6725bfc8c83155544486
Shaun Halverson <85133081+ShaunHalverson@users.noreply.github.com>
2021-12-06 11:57:56 -0600

    Create Form2.cs

---
+++ Form2.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ping_Pong
+{
+    public partial class CharacterForm : Form
+    {
+        public CharacterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void trollFaceCharacter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mogCharacter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void noCharacter_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void foreverAloneCharacter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
